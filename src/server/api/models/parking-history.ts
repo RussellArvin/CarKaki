b1f68f7aa8774ae0698ef80e9f06f4f01dc575c7
Russell Arvin <56810871+RussellArvin@users.noreply.github.com>
2024-09-17 16:11:02 +0800

    feat: add parking history profile

---
+++ src/server/api/models/parking-history.ts
@@ -0,0 +1,24 @@
+interface ParkingHistoryProps {
+    startDate: Date
+    endDate: Date
+    carParkId: string
+    userId: string
+    createdAt: Date
+    updatedAt: Date
+}
+
+export class ParkingHistory {
+    constructor(private readonly props: Readonly<ParkingHistoryProps>) {}
+
+    public getValue(): ParkingHistoryProps {
+        return { ...this.props };
+    }
+
+    public endParking(): ParkingHistory {
+        return new ParkingHistory({
+            ...this.props,
+            endDate: new Date()
+        })
+    }
+
+}
