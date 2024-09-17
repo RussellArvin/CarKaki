9a2ef91598145d0b3ce33605ad1d3e9a73147180
Russell Arvin <56810871+RussellArvin@users.noreply.github.com>
2024-09-17 16:00:45 +0800

    feat: integrate car park profile

---
+++ src/server/api/models/car-park-profile.ts
@@ -0,0 +1,46 @@
+import CarParkAgency from "../types/car-park-agency"
+import LotType from "../types/lot-type"
+
+interface CarParkProps {
+    id: string
+    area: string
+    development: string
+    latitude: number
+    longitude: number
+    availableLots: number
+    lotType: LotType
+    agency: CarParkAgency
+    hourlyRate: number
+    dailyRate: number
+    createdAt: Date
+    updatedAt: Date
+}
+
+export class CarPark {
+    constructor(private readonly props: Readonly<CarParkProps>) {}
+
+    public getValue(): CarParkProps {
+        return { ...this.props };
+    }
+
+    public updateAvailableLots(availableLots: number): CarPark {
+        return new CarPark({
+            ...this.props,
+            availableLots
+        })
+    }
+
+    public updateDailyRate(dailyRate: number): CarPark{
+        return new CarPark({
+            ...this.props,
+            dailyRate
+        })
+    }
+
+    public updateHourlyRate(hourlyRate: number): CarPark{
+        return new CarPark({
+            ...this.props,
+            hourlyRate
+        })
+    }
+}
