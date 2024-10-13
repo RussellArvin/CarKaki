ed89c801fb2012efbbf54dad430548980728e114
Russell Arvin <56810871+RussellArvin@users.noreply.github.com>
2024-10-14 03:01:57 +0800

    fix: update index schema file

---
+++ src/server/db/schema/index.ts
@@ -0,0 +1,15 @@
+import carParkSchema from "./car-park-schema";
+import parkingHistorySchema from "./parking-history-schema";
+import requestLogSchema from "./request-log-schema";
+import uraTokenSchema from "./ura-token-schema";
+import userFavouriteSchema from "./user-favourite-schema";
+import userReviewSchema from "./user-review-schema";
+import userSchema from "./user-schema";
+
+export const carPark = carParkSchema;
+export const parkingHistory = parkingHistorySchema;
+export const requestLog = requestLogSchema;
+export const uraToken = uraTokenSchema;
+export const userFavourite = userFavouriteSchema;
+export const userReview = userReviewSchema;
+export const user = userSchema;
