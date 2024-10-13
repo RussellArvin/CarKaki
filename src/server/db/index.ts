eab15f9a6f6e2be1e2d455d58aa7783509a4768f
Russell Arvin <56810871+RussellArvin@users.noreply.github.com>
2024-10-14 02:24:40 +0800

    chore: use neon db provider

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,7 +1,7 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless"
 
 import { env } from "~/env.js";
 import * as schema from "./schema";
 
-export const db = drizzle(postgres(env.DATABASE_URL));
+export const db = drizzle(neon(env.DATABASE_URL));
