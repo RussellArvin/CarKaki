e57969ef32ac9395a231808263f23ab5450e9437
Russell Arvin <56810871+RussellArvin@users.noreply.github.com>
2024-10-15 15:35:20 +0800

    chore: generate migrations

---
+++ src/server/db/migrations/0002_quick_mastermind.sql
@@ -0,0 +1,3 @@
+ALTER TYPE "lot_type_enum" ADD VALUE 'Car';--> statement-breakpoint
+ALTER TYPE "lot_type_enum" ADD VALUE 'Motorcycle';--> statement-breakpoint
+ALTER TYPE "lot_type_enum" ADD VALUE 'Heavy Vehicle';
