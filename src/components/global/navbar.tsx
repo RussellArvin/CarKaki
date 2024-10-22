417188effae24ad92bd68de08aeca9e31cb57fc1
Russell Arvin <56810871+RussellArvin@users.noreply.github.com>
2024-10-22 18:17:34 +0800

    feat: add current parking information

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -5,19 +5,22 @@ import {
     NavigationMenuLink,
     NavigationMenuList,
     navigationMenuTriggerStyle,
-  } from "~/components/ui/navigation-menu"
+} from "~/components/ui/navigation-menu"
 import APP_ROUTES from "~/lib/constants/APP_ROUTES";
-
+import { Button } from "../ui/button";
+import useUserStore from "./user-store";
 
 const Navbar = () => {
   const router = useRouter();
-    return (
-      <div className="border-b bg-blue-600">
-        <div className="flex h-16 items-center px-4">
+  const { user } = useUserStore();
+  return (
+    <div className="border-b bg-blue-600">
+      <div className="flex h-16 items-center px-4 justify-between"> {/* Added justify-between */}
+        <div className="flex items-center"> {/* Wrapped logo and nav in a div */}
           <h1 className="text-[#FFFFFF] text-3xl font-bold mr-6">CarKaki</h1>
           <NavigationMenu>
             <NavigationMenuList>
-            <NavigationMenuItem>
+              <NavigationMenuItem>
                 <NavigationMenuLink 
                   onClick={()=>router.push(APP_ROUTES.HOME)}
                   className={navigationMenuTriggerStyle()}
@@ -26,7 +29,7 @@ const Navbar = () => {
                 </NavigationMenuLink>
               </NavigationMenuItem>
               <NavigationMenuItem>
-              <NavigationMenuLink 
+                <NavigationMenuLink 
                   onClick={()=>router.push(APP_ROUTES.SETTINGS.MAIN)}
                   className={navigationMenuTriggerStyle()}
                 >
@@ -37,15 +40,28 @@ const Navbar = () => {
                 <NavigationMenuLink 
                   className={navigationMenuTriggerStyle()}
                   onClick={() => router.push(APP_ROUTES.PROFILE.MAIN)}
-                  >
+                >
                   Profile
                 </NavigationMenuLink>
               </NavigationMenuItem>
             </NavigationMenuList>
           </NavigationMenu>
         </div>
+
+        {/* Parking notification */}
+        {user?.currentParking && (
+          <Button 
+            variant="secondary"
+            className="bg-black/80 text-white hover:bg-black/70 min-w-[20rem]"
+            onClick={() => router.push(APP_ROUTES.CARPARK(user?.currentParking?.id!))}
+          >
+            <span className="mr-2">🚘</span>
+            Ongoing parking at: {user?.currentParking?.name}
+          </Button>
+        )}
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default Navbar;
