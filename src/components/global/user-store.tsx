da445fa4118a8691e67e6fc9d7505f1258ab024e
Russell Arvin <56810871+RussellArvin@users.noreply.github.com>
2024-10-23 21:49:53 +0800

    feat: add functions in store to set settings

--- src/components/global/user-store.tsx
+++ src/components/global/user-store.tsx
@@ -7,6 +7,8 @@ interface UserStore {
     user: UserDetails | null;
     isUserLoading: boolean;
     setUser: (user: UserDetails | null) => void;
+    setIsDarkMode:  (isDarkMode: boolean) => void;
+    setIsNotificationsEnabled: (isNotificationsEnabled: boolean) => void;
     setIsUserLoading: (isUserLoading: boolean) => void;
 }
 
@@ -14,6 +16,28 @@ export const useUserStore = create<UserStore>((set, get) => ({
     user: null,
     isUserLoading: false,
     setUser: (user) => set({ user }),
+    setIsDarkMode: (isDarkMode) =>{
+        const user = get().user;
+        if(!user) return;
+
+        return set({
+            user: {
+                ...user,
+                isDarkMode
+            }
+        })
+    },
+    setIsNotificationsEnabled: (isNotificationsEnabled) => {
+        const user = get().user;
+        if(!user) return;
+
+        return set({
+            user: {
+                ...user,
+                isNotificationsEnabled
+            }
+        })
+    },
     setIsUserLoading: (isUserLoading: boolean) => set({isUserLoading})
 }));
 
